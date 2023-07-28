ef3eacc156db6d6769c70a2c159325d9a1a4f939
VanessaKasei <kaseivanessa@gmail.com>
2023-07-28 21:14:23 +0300

    created add button and text area

---
+++ index.js
@@ -0,0 +1,19 @@
+const noteInput = document.getElementById('noteInput');
+const addButton = document.getElementById('addButton');
+const noteList = document.getElementById('noteList');
+
+addButton.addEventListener('click', function () {
+  const noteText = noteInput.value.trim();
+  if (noteText !== '') {
+    const li = document.createElement('li');
+    li.textContent = noteText;
+    noteList.appendChild(li);
+    noteInput.value = '';
+  }
+});
+
+noteList.addEventListener('click', function (event) {
+  if (event.target.tagName === 'LI') {
+    event.target.remove();
+  }
+});
