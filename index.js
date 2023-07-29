8680c915db609bd947d1b097b4595ca41c7d55cb
VanessaKasei <kaseivanessa@gmail.com>
2023-07-29 23:11:10 +0300

    allow add button to add new notes to the list

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const addButton = document.getElementById('addButton');
 const noteList = document.getElementById('noteList');
 
 addButton.addEventListener('click', function () {
-  const noteText = noteInput.value.trim();
-  if (noteText !== '') {
-    const li = document.createElement('li');
-    li.textContent = noteText;
-    noteList.appendChild(li);
-    noteInput.value = '';
-  }
+    const noteText = noteInput.value.trim();
+    if (noteText !== '') {
+        const li = document.createElement('li');
+        li.textContent = noteText;
+        noteList.appendChild(li);
+        noteInput.value = '';
+    }
 });
 
 noteList.addEventListener('click', function (event) {
-  if (event.target.tagName === 'LI') {
-    event.target.remove();
-  }
+    if (event.target.tagName === 'LI') {
+        event.target.remove();
+    }
 });
+
+function deleteNote(iconElement) {
+  const noteContainer = document.getElementById('noteContainer');
+  const noteToDelete = iconElement.parentNode;
+  noteContainer.removeChild(noteToDelete);
+}
